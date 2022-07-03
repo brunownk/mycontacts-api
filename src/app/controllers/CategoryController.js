62e83a131a41c463a034fa8e4f022a5bdbe8261b
Bruno Neckel <neckel.bw@gmail.com>
2022-07-03 14:14:00 -0300

    add: show, update and delete methods on category controller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,18 @@ class CategoryController {
     response.json(categories);
   }
 
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    response.json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -17,6 +29,35 @@ class CategoryController {
     const category = await CategoriesRepository.create({ name });
 
     response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const categoryExists = await CategoriesRepository.findByName(name);
+
+    if (categoryExists && categoryExists.id !== id) {
+      return response.status(400).json({ error: 'Category already exists' });
+    }
+
+    const category = await CategoriesRepository.update(id, {
+      name,
+    });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
